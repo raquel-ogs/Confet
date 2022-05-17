2ea5718db2ed2da14825cc8dd6074cadc1e25d0e
Raquel Oliveira <75819339+galaxy-oficial@users.noreply.github.com>
2022-05-17 09:08:47 -0300

    Add files via upload

---
+++ src/components/banner/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { View, TouchableOpacity, StyleSheet, Text, Image} from "react-native";
+
+export default function Banner(){
+    return(
+        <View>
+            <Image
+            style={estilo.banner} source= {require('../../img/banner2.png')}
+            />
+        </View>
+    );
+}
+
+const estilo = StyleSheet.create({
+    banner: {
+        width: "100%",
+        height: 350,
+        marginTop: -25,
+
+
+    }
+
+
+
+})
