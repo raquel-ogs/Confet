1e58338a47094572ef8696debea8005e8a95d121
Raquel Oliveira <75819339+galaxy-oficial@users.noreply.github.com>
2022-08-17 15:34:01 -0300

    Adicionando navegação no app

--- App.js
+++ App.js
@@ -1,65 +1,61 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import Home from './src/pages/home';
+import Product from './src/pages/product';
+import About from './src/pages/about';
+import Contact from './src/pages/contact';
 
-import { View, FlatList } from "react-native";
-import Cabecalho from "./src/components/cabecalho";
-import Banner from "./src/components/banner";
-import Titulo from "./src/components/titulo";
-import Produtos from "./src/components/produtos";
-import DadosProdutos from "./dados/dadosProdutos"; 
-import Lancamentos from "./src/components/lancamentos";
-import DadosLancamentos from "./dados/dadosLancamentos"; 
-import Rodape from "./src/components/rodape";
+const Tab = createBottomTabNavigator();
 
-export default function App() {
-  return (
-    <View style={{ flex:1}}>
-      <Cabecalho></Cabecalho>
-      <View>
-        <Banner> </Banner>
-      </View>
-      <View>
-        <Titulo
-          titulo =  "OS MAIS VENDIDOS"
-        />
-      </View>
-      <View>
-          <FlatList
-          horizontal={true}
-          data= {DadosProdutos}
-          keyExtractor = {(item) => item.id}
-          renderItem = { ({ item }) => (
+
+export default function App(){
+  
+
+  return(
+    
+    
+    <NavigationContainer>
+      <Tab.Navigator screenOptions={({ route }) => ({
+        headerShown: false,
+        tabBarStyle: ({height: 60}),
+        tabBarIcon: ({ focused, size, color}) => {
+          let iconName;
+          size = 30;
           
-          <Produtos
-          titulo ={item.nome}
-          imagem = {item.imagem}
-          preco = {item.preco}
-          />
-          
-          )}
-        />
-        </View>
-      <Titulo
-          titulo =  "NOVIDADES"
-        />
-       <View>
-          <FlatList
-          horizontal={true}
-          data= {DadosLancamentos}
-          keyExtractor = {(item) => item.id}
-          renderItem = { ({ item }) => (
-          
-          <Lancamentos
-          titulo ={item.nome}
-          subtitulo ={item.subtitulo}
-          desc = {item.desc}
-          imagem = {item.imagem}
-          />
-          
-          )}
-        />
-        </View>
-      <View style={{backgroundColor: "transparent"}}>
-        <Rodape></Rodape>
-      </View>
-      </View>
+          switch(route.name) {
+            case 'Home':
+              iconName = focused ? 'home-variant': 'home-variant-outline';
+              break;
+            case 'Product':
+              iconName = focused ? 'shopping': 'shopping-outline';
+              break;
+            case 'About':
+              iconName = focused ? 'account-group' : 'account-group-outline';
+              break;
+            default:
+              iconName = focused ? 'phone': 'phone-outline';
+              break;
+
+          }
+          return <MaterialCommunityIcons name={iconName} size={size} color={color} />;
+
+        },
+        
+      })}
+    tabBarOptions={{	
+      showLabel: false,
+      activeTintColor: '#black',
+      inactiveTintColor: 'gray',    
+    }}
+      >
+        <Tab.Screen name='Home' component={Home} />
+        <Tab.Screen name='Product' component={Product} />
+        <Tab.Screen name='About' component={About} />
+        <Tab.Screen name='Contact' component={Contact} />
+
+      </Tab.Navigator>
+    </NavigationContainer>
+    
   );
 }
