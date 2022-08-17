b80a69c2cfa0e5a24a308e17bc8e6134eddd0dae
Raquel Oliveira <75819339+galaxy-oficial@users.noreply.github.com>
2022-08-17 15:34:56 -0300

    Adicionando navegação no app

---
+++ src/pages/about/index.js
@@ -0,0 +1,9 @@
+import {View, Text} from 'react-native';
+
+export default function About(){
+    return(
+        <Text>
+            Sobre!
+        </Text>
+    );
+}
---
+++ src/pages/product/index.js
@@ -0,0 +1,9 @@
+import {View, Text} from 'react-native';
+
+export default function Product(){
+    return(
+        <Text>
+            Produtos!
+        </Text>
+    );
+}
